d7a3dd7feb1b171f5f18a25007f2d79affaea99f
Web-Qwelcer <74631643+Web-Qwelcer@users.noreply.github.com>
2021-05-12 00:33:16 +0300

    JS

--- js/main.js
+++ js/main.js
@@ -36,11 +36,20 @@ let $input = document.querySelectorAll('input'),
 
 for (let i = 0; i < $input.length; i++) {
     $button[i].addEventListener('click', function() {
-        if ($input[i] != list[i]) {
-            console.log('No' + $input + ' ' + list[i])
+        const input = $input[i];
+        const inputValue = new String(input.value).toLowerCase();
+        const answerValue = new String(list[i]).toLowerCase();
+
+        if (inputValue == answerValue) {
+            const inputParent = input.parentElement;
+
+            inputParent.getElementsByTagName('input')[0].remove();
+            inputParent.getElementsByTagName('button')[0].remove();
+
+            const answered = document.createElement('span');
+            answered.innerHTML = 'OK';
+            inputParent.appendChild(answered);
         }
-        else {
-            console.log('Yes' + $input + ' ' + list[i])
-        }
+        //console.log((inputValue == answerValue ? 'Yes': 'No') + ` '${inputValue}' '${list[i]}'`)
     })
 }
